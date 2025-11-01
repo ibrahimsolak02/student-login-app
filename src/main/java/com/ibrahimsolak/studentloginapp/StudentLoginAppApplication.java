bbf158c90589a1a0f2599a00626e15a7f54986d3
ibrahimsolak02 <158846254+ibrahimsolak02@users.noreply.github.com>
2025-11-01 23:20:57 +0300

    Initial commit

---
+++ src/main/java/com/ibrahimsolak/studentloginapp/StudentLoginAppApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahimsolak.studentloginapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLoginAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLoginAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibrahimsolak/studentloginapp/StudentLoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahimsolak.studentloginapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
