2ee4def29495842688d9236c35aa0e56ecab66d5
ibrahimsolak02 <158846254+ibrahimsolak02@users.noreply.github.com>
2025-11-03 00:10:53 +0300

    feat: create student entity

---
+++ src/main/java/com/ibrahimsolak/studentloginapp/student/entity/Student.java
@@ -0,0 +1,31 @@
+package com.ibrahimsolak.studentloginapp.student.entity;
+
+import com.ibrahimsolak.studentloginapp.user.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Entity
+@Table(name = "student")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @NonNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "userId", referencedColumnName = "id")
+    private User user;
+
+}
