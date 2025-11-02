39d13e45456b3f7d6288236526dc9843ccf66a29
ibrahimsolak02 <158846254+ibrahimsolak02@users.noreply.github.com>
2025-11-02 17:52:43 +0300

    feat: create ExceptionHandlerFilter class

---
+++ src/main/java/com/ibrahimsolak/studentloginapp/security/filter/ExceptionHandlerFilter.java
@@ -0,0 +1,34 @@
+package com.ibrahimsolak.studentloginapp.security.filter;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.ibrahimsolak.studentloginapp.exception.EntityNotFoundException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (EntityNotFoundException e) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.getWriter().write("Username doesn't not exist");
+            response.getWriter().flush();
+        } catch (JWTVerificationException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("JWT NOT VALID");
+            response.getWriter().flush();
+        } catch (RuntimeException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().write("BAD REQUEST");
+            response.getWriter().flush();
+        }
+    }
+}
