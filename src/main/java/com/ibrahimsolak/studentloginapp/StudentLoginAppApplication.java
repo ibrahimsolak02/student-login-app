d04bfedc675079be7881efdd8cf5a809fc086293
ibrahimsolak02 <158846254+ibrahimsolak02@users.noreply.github.com>
2025-11-08 16:24:58 +0300

    feat: create admin at start

--- src/main/java/com/ibrahimsolak/studentloginapp/StudentLoginAppApplication.java
+++ src/main/java/com/ibrahimsolak/studentloginapp/StudentLoginAppApplication.java
@@ -1,9 +1,14 @@
 package com.ibrahimsolak.studentloginapp;
 
+import com.ibrahimsolak.studentloginapp.role.Role;
+import com.ibrahimsolak.studentloginapp.user.entity.User;
+import com.ibrahimsolak.studentloginapp.user.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class StudentLoginAppApplication {
@@ -16,4 +21,18 @@ public class StudentLoginAppApplication {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public CommandLineRunner initAdmin(UserRepository userRepository, PasswordEncoder encoder) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+                User admin = new User();
+                admin.setUsername("admin");
+                admin.setPassword(encoder.encode("1234"));
+                admin.setRole(Role.ADMIN);
+                userRepository.save(admin);
+            }
+        };
+    }
+
 }
