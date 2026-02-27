0d9429501e96b61cfa291a46bccd72b55c0a2a99
ibrahimsolak02 <158846254+ibrahimsolak02@users.noreply.github.com>
2026-02-27 16:37:34 +0300

    ref: create Teacher entity

---
+++ src/main/java/com/ibrahimsolak/studentloginapp/teacher/entity/Teacher.java
@@ -0,0 +1,40 @@
+package com.ibrahimsolak.studentloginapp.teacher.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ibrahimsolak.studentloginapp.course.entity.Course;
+import com.ibrahimsolak.studentloginapp.grade.entity.Grade;
+import com.ibrahimsolak.studentloginapp.user.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "teacher")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Column(name = "name",  nullable = false)
+    @NonNull
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL)
+    private List<Grade> grades;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL)
+    private List<Course> courses;
+}
