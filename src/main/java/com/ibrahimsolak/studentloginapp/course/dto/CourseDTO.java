169d3cbb924d9c767e82baaa9319d5e8fb1bd749
ibrahimsolak02 <158846254+ibrahimsolak02@users.noreply.github.com>
2026-03-02 02:17:24 +0300

    feat: create CourseDTO

---
+++ src/main/java/com/ibrahimsolak/studentloginapp/course/dto/CourseDTO.java
@@ -0,0 +1,11 @@
+package com.ibrahimsolak.studentloginapp.course.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CourseDTO {
+    private String name;
+    private String teacherName;
+}
