37e33c68990b339b7b26efea8434624fc1ba2e42
ibrahimsolak02 <158846254+ibrahimsolak02@users.noreply.github.com>
2026-03-06 01:45:59 +0300

    feat: add findByStudentsId method

--- src/main/java/com/ibrahimsolak/studentloginapp/course/repository/CourseRepository.java
+++ src/main/java/com/ibrahimsolak/studentloginapp/course/repository/CourseRepository.java
@@ -3,5 +3,9 @@ package com.ibrahimsolak.studentloginapp.course.repository;
 import com.ibrahimsolak.studentloginapp.course.entity.Course;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends CrudRepository<Course, Long> {
+
+    List<Course> findByStudentsId(Long studentsId);
 }
