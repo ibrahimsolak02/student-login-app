266fb5e9e63517fd339fb0d85c6db6bc1114ad4a
ibrahimsolak02 <158846254+ibrahimsolak02@users.noreply.github.com>
2026-03-08 03:02:58 +0300

    feat: add findByTeacherId method

--- src/main/java/com/ibrahimsolak/studentloginapp/course/repository/CourseRepository.java
+++ src/main/java/com/ibrahimsolak/studentloginapp/course/repository/CourseRepository.java
@@ -1,11 +1,19 @@
 package com.ibrahimsolak.studentloginapp.course.repository;
 
 import com.ibrahimsolak.studentloginapp.course.entity.Course;
+import com.ibrahimsolak.studentloginapp.course.projection.CourseEnrollmentView;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CourseRepository extends CrudRepository<Course, Long> {
 
     List<Course> findByStudentsId(Long studentsId);
+
+    List<Course> findByTeacherId(Long teacherId);
+
+    @Query(value = "SELECT course_id as courseId, student_id as studentId FROM course_enrollment WHERE course_id IN :courseIds", nativeQuery = true)
+    List<CourseEnrollmentView> findEnrollmentViewsByCourseIds(@Param("courseIds") List<Long> courseIds);
 }
