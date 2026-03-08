d9de1cd6fe665a0b273570fb3ac79eaa495c56a3
ibrahimsolak02 <158846254+ibrahimsolak02@users.noreply.github.com>
2026-03-08 03:04:10 +0300

    feat: update findFirstByStudentIdAndCourseIdOrderByIdDesc method

--- src/main/java/com/ibrahimsolak/studentloginapp/grade/repository/GradeRepository.java
+++ src/main/java/com/ibrahimsolak/studentloginapp/grade/repository/GradeRepository.java
@@ -3,5 +3,9 @@ package com.ibrahimsolak.studentloginapp.grade.repository;
 import com.ibrahimsolak.studentloginapp.grade.entity.Grade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GradeRepository extends JpaRepository<Grade, Long> {
+
+    Optional<Grade> findFirstByStudentIdAndCourseIdOrderByIdDesc(Long studentId, Long courseId);
 }
